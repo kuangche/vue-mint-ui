0de33788973d15457ed50d4a785920395c056af2
jige <935506012@qq.com>
2017-06-04 11:36:08 +0800

    '修改ajax'

--- src/common/MyTools.js
+++ src/common/MyTools.js
@@ -4,9 +4,9 @@
 import Vue from 'vue';
 const config = require('../../config/index');
 
-//公共工具类----业务相关
+//公共工具�?---业务相关
 class MyTools {
-    //本地开发端口8888
+    //本地开发端�?888
     static staticUrl = window.location.port == 8888 ? './':config.staticUrl;
 
     static baseUrl = window.location.port == 8888 ? './':config.baseUrl;
@@ -14,17 +14,19 @@ class MyTools {
     static ajax(param){
         if(param.type === 'post'){
             Vue.http.post(this.baseUrl + param.url, param.data).then((res) => {
-                const result = JSON.parse(res.data);
+                const result = typeof(res.data) =='object' ? res.data : JSON.parse(res.data);
                 param.success(result);
             }, (err) => {
-                param.error(err);
+                const error = typeof(err.data) =='object' ? err.data : JSON.parse(err.data);
+                param.error(error);
             })
         }else if(param.type === 'get'){
             Vue.http.get(this.baseUrl + param.url).then((res) => {
-                const result = JSON.parse(res.data);
+                const result = typeof(res.data) =='object' ? res.data : JSON.parse(res.data);
                 param.success(result);
             }, (err) => {
-                param.error(err);
+                const error = typeof(err.data) =='object' ? err.data : JSON.parse(err.data);
+                param.error(error);
             })
         }
     }
